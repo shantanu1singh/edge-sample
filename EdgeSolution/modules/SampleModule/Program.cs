d2ac76ca4e1ddf220ec04cf17aa87d6cbd91dfc0
Shantanu Singh <shsi@microsoft.com>
2019-10-14 15:59:03 -0700

    Initial commit

---
+++ EdgeSolution/modules/SampleModule/Program.cs
@@ -0,0 +1,123 @@
+namespace SampleModule
+{
+    using System;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Runtime.Loader;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client;
+    using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+
+    class Program
+    {
+        // static int counter;
+
+        private static ModuleClient moduleClient;
+
+        private static Timer timer;
+
+        static void Main(string[] args)
+        {
+            Init().Wait();
+
+            // Wait until the app unloads or is cancelled
+            var cts = new CancellationTokenSource();
+            AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
+            Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
+            WhenCancelled(cts.Token).Wait();
+        }
+
+        /// <summary>
+        /// Handles cleanup operations when app is cancelled or unloads
+        /// </summary>
+        public static Task WhenCancelled(CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            cancellationToken.Register(s => ((TaskCompletionSource<bool>)s).SetResult(true), tcs);
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Initializes the ModuleClient and sets up the callback to receive
+        /// messages containing temperature information
+        /// </summary>
+        static async Task Init()
+        {
+            MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
+            ITransportSettings[] settings = { mqttSetting };
+
+            // Open a connection to the Edge runtime
+            moduleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
+            moduleClient.SetConnectionStatusChangesHandler(OnConnectionStatusChanged);
+            await moduleClient.OpenAsync();
+            Console.WriteLine("IoT Hub module client initialized.");
+
+            timer = new Timer(new TimerCallback(Program.SendEvent), null, 5000 /*dueTime*/, 5000 /*period*/);
+
+            // Register callback to be called when a message is received by the module
+            // await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
+        }
+
+        /// <summary>
+        /// This method is called whenever the module is sent a message from the EdgeHub. 
+        /// It just pipe the messages without any change.
+        /// It prints all the incoming messages.
+        /// </summary>
+        // static async Task<MessageResponse> PipeMessage(Message message, object userContext)
+        // {
+        //     int counterValue = Interlocked.Increment(ref counter);
+
+        //     var moduleClient = userContext as ModuleClient;
+        //     if (moduleClient == null)
+        //     {
+        //         throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
+        //     }
+
+        //     byte[] messageBytes = message.GetBytes();
+        //     string messageString = Encoding.UTF8.GetString(messageBytes);
+        //     Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
+
+        //     if (!string.IsNullOrEmpty(messageString))
+        //     {
+        //         var pipeMessage = new Message(messageBytes);
+        //         foreach (var prop in message.Properties)
+        //         {
+        //             pipeMessage.Properties.Add(prop.Key, prop.Value);
+        //         }
+        //         await moduleClient.SendEventAsync("output1", pipeMessage);
+        //         Console.WriteLine("Received message sent");
+        //     }
+        //     return MessageResponse.Completed;
+        // }
+
+        private static void SendEvent(object stateInfo)
+        {
+            try
+            {   
+                    Console.WriteLine("Sending event.");
+                    moduleClient.SendEventAsync("telemetry", new Message(Encoding.UTF8.GetBytes("abc")));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                return;
+            }
+        }
+
+        static void OnConnectionStatusChanged(ConnectionStatus status, ConnectionStatusChangeReason reason)
+        {
+            try
+            {
+                Console.WriteLine("$ConnectionStatus:{status}");
+                Console.WriteLine("$ConnectionStatusChangeReason:{reason}");
+            }
+            catch (Exception ex)
+            {
+                 Console.WriteLine(ex);
+            }
+        }
+    }
+}
